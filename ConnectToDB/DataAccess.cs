40cbbdc5920ad7fa06b9de01fe69ee3d2d97f27d
214773780 <214773780@PC5-21>
2023-10-30 19:41:50 +0200

    Add project files.

---
+++ ConnectToDB/DataAccess.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace ConnectToDB
+{
+    public class DataAccess
+    {
+
+        public void InsertDate(string IqueryString, string connectionString)
+        {
+            string BookName, Auther, Image;
+            int CategoryId, NumOfPages;
+            Console.WriteLine("insert BookName: ");
+            BookName = Console.ReadLine();
+            Console.WriteLine("insert Auther: ");
+            Auther = Console.ReadLine();
+            Console.WriteLine("insert CategoryId: ");
+            CategoryId = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("insert NumOfPages: ");
+            NumOfPages = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("insert Image: ");
+            Image = Console.ReadLine();
+
+
+            //string s = "insert into Products_tbl (ProductName,Description,CategoryId) values (@ProductName,@Description,@CategoryId)";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(IqueryString, connection);
+                command.Parameters.Add("@BookName", SqlDbType.VarChar, 20).Value = BookName;
+                command.Parameters.Add("@Auther", SqlDbType.VarChar, 10).Value = Auther;
+                command.Parameters.Add("@CategoryId", SqlDbType.Int).Value = CategoryId;
+                command.Parameters.Add("@NumOfPages", SqlDbType.Int).Value = NumOfPages;
+                command.Parameters.Add("@Image", SqlDbType.VarChar, 20).Value = Image;
+                command.Connection.Open();
+                int x = command.ExecuteNonQuery();
+                Console.WriteLine(x);
+            }
+        }
+        public  void ReadData(string RqueryString, string connectionString)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(RqueryString, connection);
+                command.Connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        Console.WriteLine("{0}\t{1}", reader.GetInt32(0), reader.GetString(1));
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("no data founded");
+                }
+                reader.Close();
+            }
+
+        }
+    }
+
+}
---
+++ ConnectToDB/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace ConnectToDB
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hi, How are you?");
+            string connectionString = "Data Source=SRV2\\PUPILS;Initial Catalog=Library214773780;Integrated Security=True";
+            string IqueryString = "insert into books (BookName,CategoryId,Auther,NumOfPages,Image) values (@BookName,@CategoryId,@Auther,@NumOfPages,@Image)";
+            string RqueryString = "select BookId,BookName from Books";
+            DataAccess Da = new DataAccess();
+            Da.InsertDate(IqueryString, connectionString);
+            
+            Da.ReadData(RqueryString, connectionString);
+
+
+        }
+    }
+}
